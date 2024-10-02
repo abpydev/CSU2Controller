32b5daa683ca3a060cc3b457eed0c5fcfb85edbb
Antoine BLASIAK <antoineblasiak66@gmail.com>
2024-10-02 18:02:59 +0200

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="csu2controller",
-    version="0.1.0",
+    version="0.1.1",
     author="Antoine BLASIAK",
     author_email="antoineblasiak66@gmail.com",
     description="A Python library to control the CSU2 device.",
